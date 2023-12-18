436e9036c877cea66bce1c7f7e6c24b4e5290c7a
Shaddy <shadybett540@gmail.com>
2023-12-18 19:37:58 +0300

    imported tools from sqlalchemy

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-# from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 # Base = declarative_base()
 
