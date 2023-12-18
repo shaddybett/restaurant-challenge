6de63cca0e73ddebce38823c3b334820ae349120
Shaddy <shadybett540@gmail.com>
2023-12-18 19:39:12 +0300

    made class of restaurant,customer and review

--- models.py
+++ models.py
@@ -3,32 +3,32 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
-# class Restaurant(Base):
-#     __tablename__ = 'restaurants'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     price = Column(Integer)
-#     reviews = relationship('Review', back_populates='restaurant')
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+    reviews = relationship('Review', back_populates='restaurant')
 
-# class Customer(Base):
-#     __tablename__ = 'customers'
-#     id = Column(Integer, primary_key=True)
-#     first_name = Column(String)
-#     last_name = Column(String)
-#     reviews = relationship('Review', back_populates='customer')
-#     def full_name(self):
-#         return f"{self.first_name} {self.last_name}"
-# class Review(Base):
-#     __tablename__ = 'reviews'
-#     id = Column(Integer, primary_key=True)
-#     star_rating = Column(Integer)
-#     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-#     customer_id = Column(Integer, ForeignKey('customers.id'))
-#     restaurant = relationship('Restaurant', back_populates='reviews')
-#     customer = relationship('Customer', back_populates='reviews')
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    reviews = relationship('Review', back_populates='customer')
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
 
-#     def full_review(self):
-#         return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
 
 # # Configure the database connection
 # DATABASE_URL = "sqlite:///restaurant.db"  # Change this based on your database configuration
